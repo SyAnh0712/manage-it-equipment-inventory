213d855e0c4d5b3e784bf773a1af22651588bdca
SyAnh0712 <syanh0712@gmail.com>
2026-06-17 18:16:50 +0700

    fix: appjs logic

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,6 +10,8 @@ const logger = require("./utils/logger");
 
 const app = express();
 
+app.set("trust proxy", 1);
+
 const allowedOrigins = [
   "http://localhost:5173",
   "https://manage-it-equipment-inventory.vercel.app",
